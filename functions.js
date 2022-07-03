fa5648992b68627ef7f243776ea5f1d130b14ecf
Thomas Song <tsong1718@gmail.com>
2022-07-03 15:58:21 -0500

    start of html and css

---
+++ functions.js
@@ -0,0 +1,7 @@
+const getQuote = document.querySelector('#quotebox')
+const textBox = document.querySelector('#textbox')
+const displayScore = document.querySelector('#display-score')
+const displayTimer = document.querySelector('#display-timer')
+const displayWpm = document.querySelector('#display-wpm')
+
+const randomQuotes = ['Pie', 'Chicken']
